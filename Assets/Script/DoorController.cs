c514fa095c6a450cae0052e0d3aaf1701304b68a
BuzzoJr <osnybuzzo@hotmail.com>
2023-10-16 12:51:43 -0300

    Cena10

--- Assets/Script/DoorController.cs
+++ Assets/Script/DoorController.cs
@@ -14,6 +14,7 @@ public class DoorController : MonoBehaviour
         C2Livingroom,
         C7Beco,
         C8ExteriorLavanderia,
+        C10Transicao,
         SampleScene
     }
     public void OnTriggerEnter(Collider other)
