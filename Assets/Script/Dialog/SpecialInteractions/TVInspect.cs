11ad78d3666fd9fbeff1dede080406f44781ec33
BuzzoJr <osnybuzzo@hotmail.com>
2023-11-07 21:59:02 -0300

    inspectTV

---
+++ Assets/Script/Dialog/SpecialInteractions/TVInspect.cs
@@ -0,0 +1,55 @@
+using Assets.Script.Dialog;
+using Assets.Script.Interaction;
+using Assets.Script.Locale;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class TVInspect : MonoBehaviour, ILook
+{
+    public bool shouldWalk = true;
+    public TextGroup textGroup = TextGroup.DialogWakeUpCall;
+    [SerializeField] private GameObject dialogBox;
+    private TMP_Text dialogText;
+
+    void Awake()
+    {
+        dialogText = dialogBox.GetComponentInChildren<TMP_Text>();
+    }
+    void ILook.Look(GameObject who)
+    {
+        StartCoroutine(CoroutineExample());
+    }
+
+    IEnumerator CoroutineExample()
+    {
+        GameManager.Instance.UpdateGameState(GameManager.GameState.Interacting);
+        PlayerController.navMeshAgent.destination = transform.position;
+        yield return null;
+        yield return new WaitUntil(() => !PlayerController.anim.GetBool("Walk"));
+
+        transform.GetChild(0).gameObject.SetActive(true);
+        dialogBox.SetActive(true);
+        foreach (TextData data in Locale.Texts[textGroup])
+        {
+            dialogText.color = TextColorManager.textTypeColors[data.Type];
+            dialogText.text = data.Type != TextType.Player ? data.Type + ": " + data.Text : data.Text;
+
+            bool clicked = false;
+            float delayTime = data.Delay > 0 ? data.Delay : 2.5f;
+            float elapsedTime = 0;
+
+            while (elapsedTime < delayTime && !clicked)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    clicked = true;
+                }
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+        dialogBox.SetActive(false);
+        GameManager.Instance.UpdateGameState(GameManager.GameState.Playing);
+    }
+}
