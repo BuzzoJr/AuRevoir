1975979a2c0672df63be42d05f12570edd39090e
Leonardo Pedroso <leoandrepedroso@gmail.com>
2023-11-13 17:10:30 -0300

    Ajuste transicao menu

--- Assets/Script/TrasitionFinal.cs
+++ Assets/Script/TrasitionFinal.cs
@@ -7,15 +7,21 @@ public class TrasitionFinal : MonoBehaviour {
     public GameObject anim;
     public GameObject credits;
     public GameObject mainCanvas;
+    public GameObject allInteract;
     public float delay = 8.12f;
 
     public void FinalGame() {
         StartCoroutine(NextScene());
     }
 
+    public void BackMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
     private IEnumerator NextScene() {
         anim.SetActive(true);
         yield return new WaitForSeconds(delay);
+        allInteract.SetActive(false);
         credits.SetActive(true);
         mainCanvas.SetActive(false);
     }
