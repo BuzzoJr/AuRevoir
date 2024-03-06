9ff87bf571392587a8fc0b23e40adad654ed7c82
DracoScript <dracoscript@gmail.com>
2024-03-06 17:37:10 -0300

    Mudando o nome do script IMORTAL

--- Assets/Script/KeepRunningOnSceneChange.cs
+++ Assets/Script/KeepRunningOnSceneChange.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IMORTAL : MonoBehaviour {
+public class KeepRunningOnSceneChange : MonoBehaviour {
 
     void Awake()
     {
