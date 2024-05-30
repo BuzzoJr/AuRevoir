028fb10e4aec4af9b911c3c56830ce3bb7b769dc
BuzzoJr <osnybuzzo@hotmail.com>
2024-05-30 13:59:13 -0300

    Sentar bar + melhorias rua

--- Assets/Script/Dialog/DialogInteraction.cs
+++ Assets/Script/Dialog/DialogInteraction.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 public class DialogInteraction : MonoBehaviour, ITalk
 {
     public bool shouldWalk = true;
+    public bool shouldSit = false;
     public TextGroup textGroup = TextGroup.DialogWakeUpCall;
     [SerializeField] private GameObject dialogBox;
     [SerializeField] private Vector3 CustomWalkOffset = Vector3.zero;
@@ -40,17 +41,20 @@ public class DialogInteraction : MonoBehaviour, ITalk
 
         if (shouldWalk)
         {
-            GameObject.FindWithTag("Player").GetComponent<PlayerController>().GoTo(new Vector3(transform.position.x + CustomWalkOffset.x, transform.position.y + CustomWalkOffset.y, transform.position.z + CustomWalkOffset.z), null);
-
+            GameObject.FindWithTag("Player").GetComponent<PlayerController>().GoTo(new Vector3(transform.position.x + CustomWalkOffset.x, transform.position.y + CustomWalkOffset.y, transform.position.z + CustomWalkOffset.z), this.transform);
             yield return null;
             yield return new WaitUntil(() => !PlayerController.anim.GetBool("Walk"));
+            if(shouldSit)
+            {
+                PlayerController.anim.SetBool("Sit", true);
+            }
         }
 
         DialogAction result = DialogAction.None;
         yield return StartCoroutine(dialog.Execute(who, (value) => result = value));
 
         GameManager.Instance.UpdateGameState(GameManager.GameState.Playing);
-
+        PlayerController.anim.SetBool("Sit", false);
         if (result == DialogAction.RemoveDialog)
             Destroy(this);
     }
