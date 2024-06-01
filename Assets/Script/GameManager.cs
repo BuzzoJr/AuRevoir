e1855ba7b6404b0f5a5cc0ead2e4d94088c28ae7
DracoScript <dracoscript@gmail.com>
2024-06-01 14:19:24 -0300

    Adicionando um cheatcode para mudar de cena

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class GameManager : MonoBehaviour
     public static event System.Action<GameState> OnGameStateChange;
 
     private AudioSource AudioInstance { get; set; }
+
+    private string goToScene = "";
 
     void Awake()
     {
@@ -31,6 +34,28 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         UpdateGameState(GameState.Playing);
+    }
+
+    void Update()
+    {
+        // Cheatcode go to scene - hold shift and input the scene number (index in build)
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            for (KeyCode k = KeyCode.Alpha0; k <= KeyCode.Alpha9; k++)
+            {
+                if (Input.GetKeyDown(k))
+                    goToScene += k.ToString().Replace("Alpha", "");
+            }
+        }
+        else if (goToScene.Length > 0)
+        {
+            Debug.Log($"Cheatcode - go to scene: {goToScene}");
+
+            if (int.TryParse(goToScene, out int scene))
+                SceneManager.LoadScene(scene);
+
+            goToScene = "";
+        }
     }
 
     public void UpdateGameState(GameState newState)
