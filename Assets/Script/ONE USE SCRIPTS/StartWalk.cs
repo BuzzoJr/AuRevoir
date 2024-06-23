0e39fc6e4e25d12a004a85a36a272e222760c1ab
BuzzoJr <osnybuzzo@hotmail.com>
2024-06-23 15:28:33 -0300

    Bathroom

--- Assets/Script/ONE USE SCRIPTS/StartWalk.cs
+++ Assets/Script/ONE USE SCRIPTS/StartWalk.cs
@@ -8,19 +8,29 @@ public class StartWalk : MonoBehaviour
     public PlayerController controller;
     public Transform destination;
     public float timeToWait = 5f;
+    public float timeToWaitPiss = 5f;
+    public AudioSource Piss;
+    public float timeToWaitRestroom = 5f;
+    public AudioSource RestroomAudio;
     void Start()
     {
         Invoke("setPosition", timeToWait);
+        Invoke("piss", timeToWaitPiss);
+        Invoke("restroom", timeToWaitRestroom);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void setPosition()
     {
         controller.GoTo(destination.position, null);
+    }
+    public void piss()
+    {
+        Piss.enabled = true;
+    }
+
+    public void restroom()
+    {
+        RestroomAudio.enabled = true;
     }
 }
